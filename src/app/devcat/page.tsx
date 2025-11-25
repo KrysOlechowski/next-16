df4ca44806ddbdeddd1dce21141801774478231e
Krystian Olechowski <krystian@MacBook-Air-Krystian.local>
2025-11-25 20:22:27 +0100

    add tree icons

--- src/app/devcat/page.tsx
+++ src/app/devcat/page.tsx
@@ -1,12 +1,79 @@
 import { randomIntFromInterval } from "@/utils/numbers";
 import Image from "next/image";
 import Tree_icon_1 from "@/components/forest/icons/Tree_1_icon.svg";
+import Tree_icon_2 from "@/components/forest/icons/Tree_2_icon.svg";
 import Tree_icon_3 from "@/components/forest/icons/Tree_3_icon.svg";
+import Tree_icon_4 from "@/components/forest/icons/Tree_4_icon.svg";
+import Tree_icon_5 from "@/components/forest/icons/Tree_5_icon.svg";
 
 const positions = [
-  0, 2, 4, 5, 7, 9, 10, 12, 13, 15, 17, 19, 20, 22, 24, 25, 27, 29, 30, 32, 34,
-  35, 37, 39, 40, 42, 44, 45, 47, 49, 50, 55, 57, 59, 60, 62, 64, 65, 67, 69,
-  70, 72, 74, 75, 77, 79, 80, 82, 84, 85, 87, 89, 90, 92, 94, 95, 97, 99, 100,
+  // [2, 2, 20],
+  // [4, 1, 16],
+  // [6, 3, 12],
+  // [8, 0, 8],
+  // [10, 4, 4],
+  // [12, 2, 0],
+  [14, 1, 20],
+  [16, 3, 16],
+  [18, 0, 12],
+  [20, 4, 8],
+  [22, 2, 4],
+  [24, 1, 0],
+  [26, 3, 20],
+  [28, 0, 16],
+  [30, 4, 12],
+  [32, 2, 8],
+  [34, 1, 4],
+  [36, 3, 0],
+  [38, 0, 20],
+  [40, 4, 16],
+  [42, 2, 12],
+
+  [45, 1, 14],
+  [44, 4, 19],
+
+  [45, 1, 14],
+  [45, 0, 4],
+  [46, 4, 22],
+  [47, 2, 6],
+  [48, 3, 14],
+  [48, 0, 0],
+  [49, 1, 4],
+  [50, 4, 20],
+  [51, 2, 8],
+  [52, 0, 4],
+  [53, 4, 15],
+  [56, 3, 8],
+  [58, 0, 4],
+  [60, 4, 0],
+  [62, 2, 20],
+  [64, 1, 16],
+  [66, 3, 12],
+  [68, 0, 8],
+  [70, 4, 4],
+  [72, 2, 0],
+  [74, 1, 20],
+  [76, 3, 16],
+  [78, 0, 12],
+  [80, 4, 8],
+  [82, 2, 4],
+  [84, 1, 0],
+  // [86, 3, 20],
+  // [88, 0, 16],
+  // [90, 4, 12],
+  // [92, 2, 8],
+  // [94, 1, 4],
+  // [96, 3, 0],
+  // [98, 0, 20],
+  // [100, 4, 16],
+];
+
+const positions_single = [
+  [25, 0, 1],
+  [53, 3, 15],
+  [50, 4, 20],
+  [47, 2, 10],
+  [75, 1, 5],
 ];
 
 const opacity = [20, 40, 60, 80, 100];
@@ -17,43 +84,53 @@ export default function Home() {
     <div className="background-green-primary h-screen">
       Dev Cat
       <div className="tree-icons-container">
-        {positions.map((position) => {
+        <div className="base"></div>
+        {positions_single.map((position) => {
           const randomTree = randomIntFromInterval(0, 1);
-          const randomSize = randomIntFromInterval(0, 4);
+          // const randomSize = randomIntFromInterval(0, 4);
+          const randomSize = position[1];
 
-          if (randomTree === 0) {
-            return (
+          const AnimationDuration = 8000 + 3000 * randomSize;
+          const top = position[2];
+
+          console.log(AnimationDuration);
+
+          return (
+            <>
               <div
-                key={position}
+                key={position[0]}
                 style={{
-                  // border: "1px solid pink",
-                  opacity: `${opacity[randomSize]}%`,
                   position: "absolute",
-                  top: randomIntFromInterval(randomSize * 2, randomSize * 5),
-                  left: `${position}%`,
+                  top: top,
+                  left: `${position[0]}%`,
+                  // animationDelay: `${AnimationDuration}ms`,
+                  animationDuration: `${AnimationDuration}ms`,
                 }}
+                className={`fade-out-element`}
               >
                 <Image height={size[randomSize]} src={Tree_icon_3} alt="Tree" />
+
+                {/* <span className="test-indicator">{position[0]}</span> */}
               </div>
-            );
-          } else {
-            return (
               <div
-                key={position}
+                key={`${position}-x`}
                 style={{
-                  // border: "1px solid pink",
-                  opacity: `${opacity[randomSize]}%`,
                   position: "absolute",
-                  top: randomIntFromInterval(randomSize * 2, randomSize * 5),
-                  left: `${position}%`,
+                  top: top,
+                  left: `${position[0] + randomIntFromInterval(1, 2)}%`,
+                  // animationDelay: `${AnimationDuration}ms`,
+                  animationDuration: `${AnimationDuration}ms`,
                 }}
+                className={`fade-in-element`}
               >
-                <Image height={size[randomSize]} src={Tree_icon_1} alt="Tree" />
+                <Image height={size[randomSize]} src={Tree_icon_2} alt="Tree" />
               </div>
-            );
-          }
+            </>
+          );
         })}
       </div>
     </div>
   );
 }
+
+// className={`animate-fade animate-duration-[10000ms] animate-ease-linear animate-reverse animate-infinite`}
