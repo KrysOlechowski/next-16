895e6bf7f472b9cdb89b6aaad42fa8e766ac1266
Krystian Olechowski <krystian@MacBook-Air-Krystian.local>
2025-11-26 20:57:25 +0100

    add hero logo

--- src/app/devcat/page.tsx
+++ src/app/devcat/page.tsx
@@ -2,10 +2,15 @@ import { ForestAnimation } from "@/components/forest/components/Forest_animation
 import Tree_icon_2 from "@/components/forest/icons/Tree_2_icon.svg";
 import Tree_icon_3 from "@/components/forest/icons/Tree_3_icon.svg";
 
+import hero_logo from "@/components/forest/images/hero_logo.svg";
+import hero_logo_png from "@/components/forest/images/image 1.png";
+
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="background-green-primary h-screen">
-      Dev Cat
+      <Image alt="Krystian" src={hero_logo_png} className="hero_logo" />
       <ForestAnimation
         position={"top"}
         icon={Tree_icon_2}
