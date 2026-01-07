09f83fc539969b8830d1d7c0be7646eb2700f8dd
Krystian Olechowski <krystian@MacBook-Air-Krystian.local>
2026-01-07 22:10:30 +0100

    add buttons on hp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import { FishkaContainer } from "@/components/fishki/Fishka_Container";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       Home Page
+      <div className="my-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+        <Button asChild variant="outline">
+          <Link href="/api-test">/api-test</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/devcat">/devcat</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/dynamic/1">/dynamic/[id]</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/game">/game</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/playground">/playground</Link>
+        </Button>
+      </div>
       <div>
         <h1>Fishki</h1>
         <div>
