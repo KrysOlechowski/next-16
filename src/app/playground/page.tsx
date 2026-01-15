a7cb00bcc5a5bbe16a7a9a55521a96bed796dc49
Krystian <kr.olechowski@gmail.com>
2026-01-15 12:01:10 +0100

    delete old supabase implementation

--- src/app/playground/page.tsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@/utils/supabase/server";
-// import { createClient } from "@/utils/supabase/client";
-export default async function English() {
-  const supabase = await createClient();
-  const { data: fishki } = await supabase.from("fishki").select();
-  return <pre>{JSON.stringify(fishki, null, 2)}</pre>;
-}
