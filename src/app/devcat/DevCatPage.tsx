00004761224baeb771fce1e4a7a6958d55d7a010
Krystian <kr.olechowski@gmail.com>
2026-01-15 02:04:37 +0100

    add files to devcat folder

--- src/app/devcat/DevCatPage.tsx
+++ src/app/devcat/DevCatPage.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import Image from "next/image";
-import { ForestAnimation } from "@/components/forest/components/Forest_animation";
-import Tree_yellow from "@/components/forest/icons/Tree_yellow.svg";
-import hero_logo from "@/components/forest/images/hero_logo.svg";
+import { ForestAnimation } from "./components/Forest_animation";
+import Tree_yellow from "./images/icons/Tree_yellow.svg";
+import hero_logo from "./images/hero_logo.svg";
 import { DevCatTestingTools } from "./components/DevCatTestingTools";
 import { useDevCatStore } from "./store/devCatStore";
 
 export const DevCatPage = () => {
   const { backgroundColor, topAnimation, bottomAnimation } = useDevCatStore();
-
+  console.log(topAnimation);
+  console.log(bottomAnimation);
   return (
-    <div className="bg-[#de982e] relative h-screen" style={{ backgroundColor }}>
+    <div className="relative h-screen" style={{ backgroundColor }}>
       <div id="bg-noise"></div>
-      <main className="relative z-10">
+      <main className="relative z-10 h-screen">
         <Image alt="Krystian" src={hero_logo} className="hero_logo" />
         <ForestAnimation
           position={"top"}
