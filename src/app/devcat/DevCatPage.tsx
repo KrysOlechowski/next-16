59b329ed0bf2ff6d1f1f51fb32850c4d9450eaee
Krystian <kr.olechowski@gmail.com>
2026-01-15 11:10:15 +0100

    add bg color to body

--- src/app/devcat/DevCatPage.tsx
+++ src/app/devcat/DevCatPage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import Image from "next/image";
 import { ForestAnimation } from "./components/Forest_animation";
 import Tree_yellow from "./images/icons/Tree_yellow.svg";
@@ -9,13 +10,27 @@ import { useDevCatStore } from "./store/devCatStore";
 
 export const DevCatPage = () => {
   const { backgroundColor, topAnimation, bottomAnimation } = useDevCatStore();
+
+  useEffect(() => {
+    document.body.style.backgroundColor = backgroundColor;
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, [backgroundColor]);
+
   console.log(topAnimation);
   console.log(bottomAnimation);
   return (
-    <div className="relative h-screen" style={{ backgroundColor }}>
+    <div className="relative min-h-screen">
       <div id="bg-noise"></div>
       <main className="relative z-10 h-screen">
         <Image alt="Krystian" src={hero_logo} className="hero_logo" />
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          harum eaque libero laboriosam commodi omnis dolore perferendis sed
+          excepturi nesciunt. Autem laudantium quo porro reprehenderit ipsa
+          numquam reiciendis molestiae! Optio?
+        </h1>
         <ForestAnimation
           position={"top"}
           icon={Tree_yellow}
